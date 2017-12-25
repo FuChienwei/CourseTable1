e8eea977c513f96e001e83b6a437ec54b01424e4
FuChienwei <34233167+FuChienwei@users.noreply.github.com>
2017-12-25 11:07:19 +0800

    1225

--- src/Table.java
+++ src/Table.java
@@ -8,11 +8,19 @@ import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 import java.awt.Window.Type;
 import javax.swing.JTextArea;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Choice;
+import javax.swing.JTextField;
 
 public class Table extends JFrame {
 
 	private JPanel contentPane;
 	private JTable table;
+	private JButton btnNewButton_2;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -22,7 +30,10 @@ public class Table extends JFrame {
 			public void run() {
 				try {
 					Table frame = new Table();
+					Table frame1=new Table();
 					frame.setVisible(true);
+					frame1.setVisible(true);
+
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -44,19 +55,18 @@ public class Table extends JFrame {
 		
 		table = new JTable();
 		
-		
 		table.setColumnSelectionAllowed(true);
 		table.setCellSelectionEnabled(true);
 	
 		table.setModel(new DefaultTableModel(
 			new Object[][] {
 				{"", "\u661F\u671F\u4E00", "\u661F\u671F\u4E8C", "\u661F\u671F\u4E09", "\u661F\u671F\u56DB", "\u661F\u671F\u4E94", "\u661F\u671F\u516D"},
-				{"\u7B2C\u4E00\u7BC0\uFF08Time\uFF09", "課程"+"\n"+"老師"+"\n"+"教室", null, null, null, null, null},
-				{"\u7B2C\u4E8C\u7BC0\uFF08Time\uFF09", null, null, null, null, null, null},
-				{"\u7B2C\u4E09\u7BC0\uFF08Time\uFF09", null, null, null, null, null, null},
-				{"\u7B2C\u56DB\u7BC0\uFF08Time\uFF09", null, null, null, null, null, null},
+				{"\u7B2C\u4E00\u7BC0\uFF08Time\uFF09", "", "\u5C08\u696D\u5916\u8A9E", "\u6578\u4F4D\u901A\u8A0A", null, null, null},
+				{"\u7B2C\u4E8C\u7BC0\uFF08Time\uFF09", "\u6578\u4F4D\u901A\u8A0A(\u5BE6)", "\u5C08\u696D\u5916\u8A9E", "\u6578\u4F4D\u901A\u8A0A", null, null, null},
+				{"\u7B2C\u4E09\u7BC0\uFF08Time\uFF09", "\u6578\u4F4D\u901A\u8A0A(\u5BE6)", null, null, null, null, null},
+				{"\u7B2C\u56DB\u7BC0\uFF08Time\uFF09", "\u6578\u4F4D\u901A\u8A0A(\u5BE6)", "\u6578\u4F4D\u901A\u8A0A", null, null, null, null},
 				{"\u7B2C\u4E94\u7BC0\uFF08Time\uFF09", null, null, null, null, null, null},
-				{"\u7B2C\u516D\u7BC0\uFF08Time\uFF09", null, null, null, null, null, null},
+				{"\u7B2C\u516D\u7BC0\uFF08Time\uFF09", null, "\u73ED\u6703", null, null, null, null},
 				{"\u7B2C\u4E03\u7BC0\uFF08Time\uFF09", null, null, null, null, null, null},
 				{"\u7B2C\u516B\u7BC0\uFF08Time\uFF09", null, null, null, null, null, null},
 				{"\u7B2C\u4E5D\u7BC0\uFF08Time\uFF09", null, null, null, null, null, null},
@@ -73,8 +83,65 @@ public class Table extends JFrame {
 		table.getColumnModel().getColumn(5).setPreferredWidth(100);
 		table.getColumnModel().getColumn(6).setPreferredWidth(100);
 		table.setDefaultRenderer(Object.class, new TableCellTextAreaRenderer());
-		table.setBounds(10, 10, 702, 610);
-		contentPane.add(table);
-	}
+		table.setBounds(10, 65, 702, 497);
+		
 	
+		contentPane.add(table);
+		
+		JButton btnNewButton = new JButton("確定修改");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				
+			}
+		});
+		
+		btnNewButton.setBounds(481, 21, 87, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("確定修改");
+		btnNewButton.addActionListener(new ActionListener(){
+
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+		btnNewButton_1.setBounds(578, 584, 87, 23);
+		contentPane.add(btnNewButton_1);
+		
+		btnNewButton_2 = new JButton("New button");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		btnNewButton_2.setBounds(311, 584, 87, 23);
+		contentPane.add(btnNewButton_2);
+		
+		Choice choice = new Choice();
+		choice.setBounds(58, 21, 93, 21);
+		choice.add("星期一");
+		choice.add("星期二");
+		choice.add("星期三");
+		choice.add("星期四");
+		choice.add("星期五");
+		choice.add("星期六");
+		contentPane.add(choice);
+		
+		Choice choice_2 = new Choice();
+		choice_2.setBounds(201, 21, 93, 21);
+		contentPane.add(choice_2);
+		
+		textField = new JTextField();
+		textField.setBounds(58, 585, 96, 21);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(336, 22, 96, 21);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+	}
 }
--- src/TableCellTextAreaRenderer.java
+++ src/TableCellTextAreaRenderer.java
@@ -1,11 +1,16 @@
 import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import javax.swing.*;
 import javax.swing.table.*;
 import java.awt.*;
 import javax.swing.JTable;
 import javax.swing.table.TableCellRenderer;
 
-public class TableCellTextAreaRenderer extends JTextArea implements TableCellRenderer {
+public class TableCellTextAreaRenderer extends JTextArea implements TableCellRenderer,MouseListener {
 
 	@Override
 	 public Component getTableCellRendererComponent(JTable table, Object value,   
@@ -20,7 +25,7 @@ public class TableCellTextAreaRenderer extends JTextArea implements TableCellRen
 	       }   
 	         
 	       if (table.getRowHeight(row) != maxPreferredHeight)  
-	         table.setRowHeight(60);   
+	         table.setRowHeight(50);   
 	         
 	         setFont(new Font("微軟正黑體", Font.PLAIN, 14));  
 	         setText(value == null ? "" : value.toString());   
@@ -34,6 +39,31 @@ public class TableCellTextAreaRenderer extends JTextArea implements TableCellRen
 	 public TableCellTextAreaRenderer() {   
 	       setLineWrap(true); 
 	       setWrapStyleWord(true); 
-	   }   
+	   }
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("xxxxxxxxxxxxxxxxxxx");
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("xxxxxxxxxxxxxxxxxxx");
+	}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("xxxxxxxxxxxxxxxxxxx");
+	}
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("xxxxxxxxxxxxxxxxxxx");
+	}
+	
 	  
 }
